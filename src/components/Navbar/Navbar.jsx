eebe9f62e0a2c3a5a8b1523d0355c25ed065a158
Aman Harsh <amanharsh0399@gmail.com>
2023-07-29 20:57:03 +0530

    fix: logo link
    fix: logo link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import { IoNotifications as NotificationIcon } from "react-icons/io5";
 import { SearchBar } from "../index";
+import { Link } from "react-router-dom";
 
 export function Navbar() {
   return (
     <nav className="flex justify-between items-center py-2 pb-6">
-      <h2>SnapGOD</h2>
+      <Link to="/">
+        <h2>SnapGOD</h2>
+      </Link>
       <SearchBar />
       <NotificationIcon size={24} className="cursor-pointer fill-gray-300" />
     </nav>
